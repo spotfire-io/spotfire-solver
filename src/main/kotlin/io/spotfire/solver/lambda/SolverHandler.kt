72842a2209f827cdda623c591a274bdf65fbf2e4
Cedric Hurst <cedric@spantree.net>
2019-09-25 01:06:22 -0500

    addressing an eventual consistency issue with s3 uploads

--- src/main/kotlin/io/spotfire/solver/lambda/SolverHandler.kt
+++ src/main/kotlin/io/spotfire/solver/lambda/SolverHandler.kt
@@ -45,72 +45,94 @@ class SolverHandler : RequestHandler<Map<String, Any>, Map<String, Any>> {
   }
 
   private fun downloadAndExtractFile(path: String): File {
-    logger.info("Extracting file from ${path}")
+    logger.info("Extracting file from $path")
     getExtractHashFromPath(path)!!.let { extractHash ->
       val extractDir = File("/tmp/spotfire-extracts/$extractHash")
-      if (!extractDir.exists()) {
+      if (!extractDir.exists() || extractDir.list().isEmpty()) {
         extractDir.mkdirs()
-        val extractReq = Request.Builder().url(path).build()
-        val extractResp = httpClient.newCall(extractReq).execute()
-        val tmpFile = createTempFile(suffix = ".tar.gz")
+        var attempts = 0
+        while(attempts++ < 5) {
+          val extractReq = Request.Builder().url(path).build()
+          val extractResp = httpClient.newCall(extractReq).execute()
 
-        extractResp.body()?.byteStream()?.use { bodyStream ->
-          tmpFile.outputStream().use { tmpFileStream ->
-            bodyStream.copyTo(tmpFileStream)
+          if(extractResp.code() != 200) {
+            val pauseMs = 500*Math.pow(2.toDouble(), attempts.toDouble()).toLong()
+            logger.warn("Received status code ${extractResp.code()} when attempting to fetch $path, attempt #${attempts}. Pausing ${pauseMs} before retrying.")
+            Thread.sleep(pauseMs)
+          } else {
+            val tmpFile = createTempFile(suffix = ".tar.gz")
+            extractResp.body()?.byteStream()?.use { bodyStream ->
+              tmpFile.outputStream().use { tmpFileStream ->
+                bodyStream.copyTo(tmpFileStream)
+              }
+              logger.info("Wrote extract file to $tmpFile")
+              logger.info("Extracted file to $extractDir")
+              extractToDir(tmpFile, extractDir)
+            } ?: run {
+              throw Exception("Could not get bytestream from extract file $path")
+            }
+            break
           }
-          logger.info("Wrote extract file to $tmpFile")
-          logger.info("Extracted file to $extractDir")
-          extractToDir(tmpFile, extractDir)
-        } ?: run {
-          throw Exception("Could not get bytestream from extract file $path")
+        }
+        if(attempts > 5) {
+          throw Exception("Could not retrieve file from path $path")
         }
       }
+
       return extractDir
     }
   }
 
   override fun handleRequest(input: Map<String, Any>, context: Context): Map<String, Any> {
-    val jobMap = input.get("job") as HashMap<String, String>
-    logger.info("received input for request foo $input")
-    jobMap["id"]?.let { jobId ->
-      jobMap["extractPath"]?.let { extractPath ->
-        val accessToken = input["accessToken"] as String?
-        val graphqlEndpointURL = input["graphqlEndpointURL"] as String?
-        val job = OptimizationJob(id = jobId, extractPath = extractPath)
+    try {
+      val jobMap = input.get("job") as HashMap<String, String>
+      logger.info("received input for request foo $input")
+      jobMap["id"]?.let { jobId ->
+        jobMap["extractPath"]?.let { extractPath ->
+          val accessToken = input["accessToken"] as String?
+          val graphqlEndpointURL = input["graphqlEndpointURL"] as String?
+          val job = OptimizationJob(id = jobId, extractPath = extractPath)
 
-        val extractDir = downloadAndExtractFile(extractPath)
-        logger.info("Building problem from extract folder")
-        val builder = ProblemBuilder(extractDir.absolutePath)
-        val problem = builder.build()
+          val extractDir = downloadAndExtractFile(extractPath)
+          logger.info("Building problem from extract folder")
+          val builder = ProblemBuilder(extractDir.absolutePath)
+          val problem = builder.build()
 
-        logger.info("Starting solver")
-        val solver = PlaylistSolverFactory().getSolver(problem)
-        if(graphqlEndpointURL != null && accessToken != null) {
-          val listener = UpdateJobSolverEventListener(solver, job, graphqlEndpointURL, accessToken)
-          solver.addEventListener(listener)
-          if (solver is AbstractSolver) {
-            solver.addPhaseLifecycleListener(listener)
+          logger.info("Starting solver")
+          val solver = PlaylistSolverFactory().getSolver(problem)
+          if (graphqlEndpointURL != null && accessToken != null) {
+            val listener = UpdateJobSolverEventListener(solver, job, graphqlEndpointURL, accessToken)
+            solver.addEventListener(listener)
+            if (solver is AbstractSolver) {
+              solver.addPhaseLifecycleListener(listener)
+            }
+          } else {
+            logger.warn("Not adding update listener bc endpoint or access token not provided in payload")
           }
-        } else {
-          logger.warn("Not adding update listener bc endpoint or access token not provided in payload")
+
+
+          val solution = solver.solve(problem)
+
+          logger.info("Finished solving in ${solver.timeMillisSpent} with best score of ${solver.bestScore.toShortString()}")
+
+          val status = SolverStatus.fromSolution(solver, solution)
+
+          return mapOf(
+            "statusCode" to 200,
+            "body" to solverStatusAdapter.toJson(status)
+          )
+        } ?: run {
+          throw Exception("Extract path not provided for job input")
         }
-
-
-        val solution = solver.solve(problem)
-
-        logger.info("Finished solving in ${solver.timeMillisSpent} with best score of ${solver.bestScore.toShortString()}")
-
-        val status = SolverStatus.fromSolution(solver, solution)
-
-        return mapOf(
-          "statusCode" to 200,
-          "body" to solverStatusAdapter.toJson(status)
-        )
       } ?: run {
-        throw Exception("Extract path not provided for job input")
+        throw Exception("Job ID not provided for job input")
       }
-    } ?: run {
-      throw Exception("Job ID not provided for job input")
+    } catch(e: Exception) {
+      logger.error("An error occurred", e)
+      return mapOf(
+        "statusCode" to 500,
+        "body" to "An error occurred: ${e.message}"
+      )
     }
   }
 }
